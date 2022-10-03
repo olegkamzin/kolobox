62ba88d81bbd572352cdb7c111d0c2842c5bae17
Kamzin <olegkamzin@gmail.com>
2022-10-03 08:06:05 +0300

    auth

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 import Router from 'express'
 import ProductController from '../controllers/product.js'
+import auth from '../middleware/auth.js'
 const router = new Router()
 
-router.get('/', ProductController.get)
+router.get('/', auth, ProductController.get)
 
 export default router
