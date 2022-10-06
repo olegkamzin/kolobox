bc1fd1d6b383c1f03512812ab5cde564c673e485
Kamzin <olegkamzin@gmail.com>
2022-10-07 00:50:03 +0300

    fix

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-	origin: process.env.CLIENT_URL,
-	credentials: true
-}))
+app.use(cors())
 app.use(router)
 app.use(error)
 
