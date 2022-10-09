1134eec3a44c209851a41babc1b356e235a8595b
Kamzin <olegkamzin@gmail.com>
2022-10-10 00:39:16 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import products from './products.js'
 
 const router = new Router()
 
-router.use('/service/kolobox/orders', orders)
-router.use('/service/kolobox/products', products)
+router.use('/kolobox/orders', orders)
+router.use('/kolobox/products', products)
 
 export default router
