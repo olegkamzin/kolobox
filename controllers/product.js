48203c470fe83fea32d0ff230d513bf537727604
Kamzin <olegkamzin@gmail.com>
2022-11-15 13:30:51 +0300

    fix

--- controllers/product.js
+++ controllers/product.js
@@ -7,7 +7,7 @@ class ProductController {
 	async get (req, res, next) {
 		try {
 			const { id } = req.query
-			const product = await axios.get('https://api.shinpi.ru/product/?id=' + id)
+			const product = await axios.get('https://api.shinpi.ru/product/' + id)
 			return await getTyres(product.data.brand.name, product.data.article)
 				.then(result => res.send(result.data[0]))
 				.catch(async error => {
