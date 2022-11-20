e385832f70cc8707841fdd1709b33ddf44559355
Kamzin <olegkamzin@gmail.com>
2022-11-21 00:30:50 +0300

    retail price

--- service/koloboxTyres.js
+++ service/koloboxTyres.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getTyres = async (brand, articul) => {
-	return await axios.get(process.env.KOLOBOX_URL + 'catalog/tyres/0/?articul=' + articul + '&mark=' + brand, {
+	return await axios.get(process.env.KOLOBOX_URL + 'catalog/tyres/0/?pricetype=retail&articul=' + articul + '&mark=' + brand, {
 		headers: { Authorization: 'Bearer ' + globalThis.token }
 	})
 }
