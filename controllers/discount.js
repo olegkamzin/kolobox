7cc11e48849a7d1a4b058010fbdd3c41de8748dc
Kamzin <olegkamzin@gmail.com>
2023-01-05 23:34:37 +0300

    Исправление

--- controllers/discount.js
+++ controllers/discount.js
@@ -6,21 +6,21 @@ class DiscountController {
 	async get (req, res, next) {
 		try {
 			return getDiscount()
-				.then(res => {
-					return res.json(res.data)
+				.then(result => {
+					return res.send(result.data)
 				})
 				.catch(err => {
-					if (err.response.status === 401) {
+					if (err.response?.status === 401) {
 						return koloboxAuth().then(() => {
 							return getDiscount()
 								.then(result => res.send(result.data))
 								.catch(error => next(ApiError.badRequest(error)))
 						})
 					}
-					return next(ApiError.badRequest(err))
+					return next(ApiError.badRequest(err.message))
 				})
 		} catch (e) {
-			return next(ApiError.badRequest(e))
+			return next(ApiError.badRequest(e.message))
 		}
 	}
 }
